d52df7e9286949e6373fd757ec9ce499a93f2477
Kartik Anand <kartik@MacBookAir.lan>
2026-08-31 11:12:50 +0100

    Add cost_of_equity function and test

--- finance/dcf.py
+++ finance/dcf.py
@@ -1,4 +1,10 @@
 # Building the cost of equity code lines
-def cost_of_equity(risk_free_rate, beta, equity_risk_premium):
+def cost_of_equity(risk_free_rate:float, beta:float, equity_risk_premium:float):
     cost_of_equity = risk_free_rate + beta * equity_risk_premium
     return cost_of_equity
+
+
+#Building cost of debt code lines
+def after_tax_cost_of_debt(pre_tax_cost_of_debt:float, tax_rate:float):
+    after_tax_cost_of_debt = pre_tax_cost_of_debt * (1 - tax_rate)
+    return after_tax_cost_of_debt
--- tests/test_dcf.py
+++ tests/test_dcf.py
@@ -1,7 +1,11 @@
 #Tests for all code functions 
 import pytest
-from finance.dcf import cost_of_equity
+from finance.dcf import cost_of_equity, after_tax_cost_of_debt
 
 #cost_of_equity test
 def test_cost_of_equity():
     assert cost_of_equity(0.04, 1.2, 0.05) == pytest.approx(0.1)
+
+#after_tax_cost_of_debt test
+def test_after_tax_cost_of_debt():
+    assert after_tax_cost_of_debt(0.06, 0.25) == pytest.approx(0.045)

0b57318d11fedfd6ea5d9ae60d7dd96a516d430d
Kartik Anand <kartik@MacBookAir.lan>
2026-08-31 11:27:57 +0100

    Add wacc function and test

--- finance/dcf.py
+++ finance/dcf.py
@@ -8,3 +8,11 @@ def cost_of_equity(risk_free_rate:float, beta:float, equity_risk_premium:float):
 def after_tax_cost_of_debt(pre_tax_cost_of_debt:float, tax_rate:float):
     after_tax_cost_of_debt = pre_tax_cost_of_debt * (1 - tax_rate)
     return after_tax_cost_of_debt
+
+#Building wacc code lines
+def wacc(cost_of_equity:float, cost_of_debt:float, market_value_of_equity:float, market_value_of_debt:float):
+    total_value = market_value_of_equity + market_value_of_debt
+    if total_value == 0:
+        raise ValueError("Total value of equity and debt cannot equal 0")
+    wacc = (cost_of_equity * (market_value_of_equity / total_value) + cost_of_debt * (market_value_of_debt / total_value))
+    return wacc
--- tests/test_dcf.py
+++ tests/test_dcf.py
@@ -1,6 +1,6 @@
 #Tests for all code functions 
 import pytest
-from finance.dcf import cost_of_equity, after_tax_cost_of_debt
+from finance.dcf import cost_of_equity, after_tax_cost_of_debt, wacc
 
 #cost_of_equity test
 def test_cost_of_equity():
@@ -9,3 +9,7 @@ def test_cost_of_equity():
 #after_tax_cost_of_debt test
 def test_after_tax_cost_of_debt():
     assert after_tax_cost_of_debt(0.06, 0.25) == pytest.approx(0.045)
+
+#wacc test
+def test_wacc():
+    assert wacc(0.1, 0.045, 800, 200) == pytest.approx(0.089)
